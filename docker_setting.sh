7b99bfd695ab8efec225350d1ceb13a620019c1f
Leon <leon@Leons-MacBook-Pro.local>
2020-05-06 13:36:02 +0800

    add basic setting of docekr

---
+++ docker_setting.sh
@@ -0,0 +1,12 @@
+# acquire image of ubuntu after intstall docker
+docker pull ubuntu
+# creat container with mounting ~/Gallopwave 
+docker run -it -v ~/Gallopwave:/Gallopwave --name mac_ubuntu ubuntu /bin/bash
+
+# ps: docker ps -a (check all existed container)
+# open container: ${container_name}
+docker start ${container_name} -i 
+
+#multiple terminals of docker container
+docker exec -it ${container_name} bash
+
