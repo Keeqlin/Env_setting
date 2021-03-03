d6d48e852f3c71c83daf0ee81f204294ca4ca5fd
Keeqlin <50442496+Keeqlin@users.noreply.github.com>
2021-03-03 19:10:16 +0800

    Update Mac_env_setting.sh

--- Mac_env_setting.sh
+++ Mac_env_setting.sh
@@ -64,7 +64,7 @@ git clone https://github.com/opencv/opencv_contrib.git
 cd ~/opencv/opencv
 mkdir build
 cd build
-cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=/usr/local -D OPENCV_EXTRA_MODULES_PATH=~/opencv/opencv_contrib/modules/ ../
+cmake -D CMAKE_BUILD_TYPE=Release -DBUILD_JPEG_TURBO_DISABLE=ON -DBUILD_TESTS=OFF -D CMAKE_INSTALL_PREFIX=/usr/local -D OPENCV_EXTRA_MODULES_PATH=~/opencv/opencv_contrib/modules/ ../
 make -j7
 make install
 
